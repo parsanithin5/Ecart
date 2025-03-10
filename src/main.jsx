b36039a38f787b9e17a1843d6c5fe6575edf2b9f
parsanithin5 <pparsanithin@gmail.com>
2025-03-10 11:40:12 +0530

    adding files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import Master from './Master.jsx'
 createRoot(document.getElementById('root')).render(
-   <Master></Master>,
+   <Master/>
 )
